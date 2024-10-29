aee0bf6d75131d07df129fe65279960fa64a422a
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-10-29 08:01:12 -0300

    feat: update to connection.js

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize')
 //* Database configuration values
 const database = 'todo_db'
 const username = 'todo_user'
-const password = 'password'
+const password = process.env.DB_PASSWORD
 const host = process.env.HOSTNAME
 
 const sequelize = new Sequelize(database, username, password, {
